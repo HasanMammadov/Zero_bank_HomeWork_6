4539dc429cf02e54b42bd90c4bd5237ba49826c1
hasan <hasanmamedoff@gmail.com>
2020-04-28 14:45:09 -0400

    first commit

---
+++ src/test/java/com/zerobank/runners/FailedRunner.java
@@ -0,0 +1,27 @@
+package com.zerobank.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+        features ="@target/rerun.txt" ,
+        glue = "com/zerobank/stepdefnitions",
+
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json"
+        }
+
+
+
+
+
+)
+
+public class FailedRunner {
+}
